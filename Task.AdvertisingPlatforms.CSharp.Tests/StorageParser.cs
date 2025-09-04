9c45798aed081ae2bfa6957a7d3f1bcea64ded39
Serhii Novytskyi <novickii.sergei.nure@gmail.com>
2025-09-04 20:59:56 +0300

    Расширение тестов

--- Task.AdvertisingPlatforms.CSharp.Tests/StorageParser.cs
+++ Task.AdvertisingPlatforms.CSharp.Tests/StorageParser.cs
@@ -37,7 +37,14 @@ public class StorageTest
         var content_ok = File.ReadAllText("../../../../TestFiles/File1_ok");
         var data = _parser.Parse(content_ok);
         _storage.Add(data);
-        var result = _storage.GetPlatforms("/ru");
-        Assert.That(result.Count, Is.EqualTo(1));
+        
+        var result1 = _storage.GetPlatforms("/ru/msk");
+        Assert.That(result1.Count, Is.EqualTo(2));
+        var result2 = _storage.GetPlatforms("/ru/svrd");
+        Assert.That(result2.Count, Is.EqualTo(2));
+        var result3 = _storage.GetPlatforms("/ru/svrd/revda");
+        Assert.That(result3.Count, Is.EqualTo(3));
+        var result4 = _storage.GetPlatforms("/ru");
+        Assert.That(result4.Count, Is.EqualTo(1));
     }
 }
